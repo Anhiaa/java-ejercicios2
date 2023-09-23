f76498fe550ccced2a017905d46fed04a3744e4b
Anhiaa <140754260+Anhiaa@users.noreply.github.com>
2023-09-23 14:16:09 -0600

    Add files via upload

---
+++ ayb.java
@@ -0,0 +1,16 @@
+public class a y b {  
+    public static void main(String[] args) {
+
+    
+
+    String cadena = "Hola me llamo Anhia";
+        if(cadena.contains("a")&&
+        cadena.contains("b")) {
+            System.out.println("la cadena contiene las letras 'a' y b");
+        }else{
+            System.out.println("la cadena no contiene las letras 'a' y 'b");
+        }
+    }
+}
+
+
